e97f6e499f741cf5fc64c1c061acdad1b71e7f95
wql <3014248544@qq.com>
2021-12-28 20:44:47 +0800

    hot-fix

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,9 +3,7 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("hello git2");
-        System.out.println("hello git3");
-        System.out.println("hello git4 hot-fix");
+        System.out.println("hot-fix");
 
     }
 }
